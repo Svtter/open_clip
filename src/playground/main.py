eb085416a3d36b460f02c043f060467167c5ee1a
svtter <svtter@qq.com>
2024-07-24 12:00:31 +0800

    just format code, via ruff.

--- src/playground/main.py
+++ src/playground/main.py
@@ -1,10 +1,16 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+
 import torch
 from PIL import Image
 import open_clip
 
-model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+model, _, preprocess = open_clip.create_model_and_transforms(
+    "ViT-B-32", pretrained="laion2b_s34b_b79k"
+)
 model.eval()  # model in train mode by default, impacts some models with BatchNorm or stochastic depth active
-tokenizer = open_clip.get_tokenizer('ViT-B-32')
+tokenizer = open_clip.get_tokenizer("ViT-B-32")
 
 image = preprocess(Image.open("docs/CLIP.png")).unsqueeze(0)
 text = tokenizer(["a diagram", "a dog", "a cat"])
